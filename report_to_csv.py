0020b1e7a82ccd46c82facec7620bd6ab944778b
Armstrong Goes <armstrongmsg@gmail.com>
2020-01-27 12:25:28 -0300

    Added script to convert json reports to csv

---
+++ report_to_csv.py
@@ -0,0 +1,22 @@
+import json
+import time
+import datetime
+import sys
+import ntpath
+
+print("filename,timestamp,error,replicas,job_progress,time_progress")
+
+for file_path in sys.argv[1:]:
+    js_file = open(file_path, "r")
+    report_dict = json.load(js_file)
+    file_name = ntpath.basename(file_path)
+
+    for key in sorted(report_dict):
+        timestamp = time.mktime(datetime.datetime.strptime(key,"%Y-%m-%dT%H:%M:%SZ").timetuple())
+        job_progress = report_dict[key]['job_progress']
+        error = report_dict[key]['error']
+        time_progress = report_dict[key]['time_progress']
+        replicas = report_dict[key]['replicas']
+        print("%s,%d,%f,%d,%f,%f" % (file_name, timestamp, error, replicas, job_progress, time_progress))
+
+    js_file.close()
