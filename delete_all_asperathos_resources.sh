397bbf64f2de94e965a040f1a36d855565863af1
Armstrong Goes <armstrongmsg@gmail.com>
2020-03-06 18:17:50 -0300

    Added redis pods and services to the deleted resources

--- delete_all_asperathos_resources.sh
+++ delete_all_asperathos_resources.sh
@@ -2,9 +2,9 @@
 
 KUBE_CONF_FILE=$1
 
-services=`kubectl --kubeconfig=$KUBE_CONF_FILE get services | awk '{print $1}' | grep -E "influx|grafana"`
+services=`kubectl --kubeconfig=$KUBE_CONF_FILE get services | awk '{print $1}' | grep -E "influx|grafana|redis"`
 
-pods=`kubectl --kubeconfig=$KUBE_CONF_FILE get pods | awk '{print $1}' | grep -E "influx|grafana"`
+pods=`kubectl --kubeconfig=$KUBE_CONF_FILE get pods | awk '{print $1}' | grep -E "influx|grafana|redis"`
 
 kubectl --kubeconfig=$KUBE_CONF_FILE delete services $services
 kubectl --kubeconfig=$KUBE_CONF_FILE delete pods $pods
